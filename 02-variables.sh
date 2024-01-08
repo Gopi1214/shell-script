dadb993967b98e4e136655de94173a47db8a241e
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-08 19:35:49 +0530

    added variables script

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#general conversation
+
+echo "Ramesh: Hi Suresh, Good Morning"
+echo "Suresh: Hello Ramesh, Very Good Morning"
+echo "Ramesh: How are you doing sureh?"
+echo "Suresh: I am doing good what about you ramesh?"
