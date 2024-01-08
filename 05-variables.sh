df4a5ca1159a98d8e064a5dcee503bad83a69428
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-08 22:10:16 +0530

    hiding username and passwd

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter your USERNAME:"
+
+read -s USERNAME 
+
+echo "Please enter yuor PASSWORD:"
+
+read -s PASSWORD
+
+echo " Your USERNAME is :$USERNAME, Your PASSWORD is : $PASSWORD"  #This is just for reference only usually we will not print the username and passwords
