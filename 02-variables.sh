670af215eb1b9795a4c0570dc34f46a2ea305062
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-08 21:23:40 +0530

    updated variable names

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,10 +2,10 @@
 
 #general conversation
 
-PERSON1=$Ramesh
-PERSON2=$Suresh
+PERSON1=Ramesh
+PERSON2=Suresh
 
 echo "$PERSON1: Hi $PERSON2, Good Morning"
 echo "$PERSON2: Hello $PERSON1, Very Good Morning"
 echo "$PERSON1: How are you doing $PERSON2?"
-echo "$PERSON2: I am doing good what about you $PERSON1?"
+echo "$PERSON2: I am doing good, what about you $PERSON1?"
