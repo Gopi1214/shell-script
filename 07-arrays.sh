911d54575f2f167a0f52358c33508a6959d28696
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-10 13:57:12 +0530

    corrected the syntax

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-FRUITS=("Aplle", "Banana", "Mango")
+FRUITS=("Apple", "Banana", "Mango")
 
-echo "First Fruit is : {$FRUITS[0]}"
+echo "First Fruit is : ${FRUITS[0]}"
 
-echo "Second Fruit is : {$FRUITS[1]}"
+echo "Second Fruit is : ${FRUITS[1]}"
 
-echo "Third Fruit is : {$FRUIT[2]}"
+echo "Third Fruit is : ${FRUITS[2]}"
 
-echo "All fruits in the array are : {$FRUITS[@]}"
+echo "All fruits in the array are : ${FRUITS[@]}"
