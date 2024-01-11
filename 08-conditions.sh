aa41a636e5bde9c026105924d3251bef1a744407
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-11 20:06:37 +0530

    passing number as dynamic

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$1
 
-if [ $NUMBER -gt 100 ];
+if [ $NUMBER  -gt  100 ]
 then
-    echo "given $NUMBER is greater than 100"
+echo "given $NUMBER is greater than 100"
 else
-    echo "given $NUMBER is not greater than 100"
+echo "given $NUMBER is not greater than 100"
 fi
