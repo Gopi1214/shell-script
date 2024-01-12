b5ed105cfbe32490c552b0d226529f5c0f20fc38
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-12 07:04:08 +0530

    comments

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -4,10 +4,20 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
-  echo "user has no root acces, so not allwed to install mysql."
+    echo "ERROR:: user has no root acces, so not allwed to install mysql."
+    exit 1 #you can give other than 0
 else
-  echo "root user install mysql."
+    echo "root user install mysql."
    
 fi #fi is reverse of if, indicating the end of condition
 
 yum install mysql -y
+
+if [ $? != 0 ]
+then
+    echo "ERROR:: installation was failed."
+    exit 1
+else
+    echo "installation was success".
+fi
+
