4d010a9216d971e27a16ff67f2029d9d8097723a
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-12 11:56:46 +0530

    crated log script

---
+++ 11-log.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo "Script name: $0"
+
+VALIDATE(){
+    if [ $1 != 0 ]
+    then
+        echo "ERROR::$2 was....FAILED."
+        exit 1
+    else
+        echo "$2....was SUCCESS."
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "user has no root access and not able to install."
+    exit 1
+
+else
+    echo "user has root access."
+fi
+
+yum install mysql -y
+
+VALIDATE $? "Installing MySql"
+
+yum install git -y
+
+VALIDATE $? "Installing Git"
