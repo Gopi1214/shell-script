08ba088802b5cf894f2948de00436c60a87c96e6
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-14 10:28:22 +0530

    loops conscept for installing any number of packages

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 
 if [ $ID -ne 0 ]
 then 
-    echo -e " $R ERROR:: $N user has no root acces, not able to install or execute commands"
+    echo -e "$R ERROR:: $N user has no root acces, not able to install or execute commands"
 
 else 
     echo "user has root acces"
