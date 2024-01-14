c9e01c72bda79b3139e0d2d43f6db0cd4860b5cb
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-14 12:30:37 +0530

    loops conscept for installing any number of packages

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -4,7 +4,24 @@ ID=$(id -u)
 
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+
+TIMESTAMP=$(date+%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started running at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 was ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 was ...$G SUCCESS $N"
+    fi
+}
+
 
 
 if [ $ID -ne 0 ]
@@ -16,4 +33,20 @@ else
 
 fi
 
-echo "All argumets passed: $@"
+#echo "All argumets passed: $@"
+#git mysql net-tools nginx
+#package=git for first time
+
+for package in $@
+do 
+    yum list installed $package &>> $LOGFILE
+    if [ $? -ne 0 ]
+    then 
+        yum install $package -y &>> $LOGFILE
+        VALIDATE $? "Insatallationof $package"
+
+    else
+        echo -e "$package is already installed...$Y SKIPPING $N"
+    fi
+done
+
