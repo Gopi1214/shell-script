0df64eb265412bde3d90a1a96b765ae8d605f043
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-22 19:24:29 +0530

    shell script for deleting old logs

---
+++ 15-delete-old-log.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SOURCE_DIR="/tmp/shellscript-logs"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+if [ ! -d $SOURCE_DIR ]
+then
+    echo "$R ERROR: source directory $SOURCE_DIR does not exists$N"
+
+fi
+
+FIND_OLD_FILES=$(find $SOURCE_DIR -type f -mtime +14 "*.log")
+
+while IFS=read -r line; 
+do
+    echo "Delete files: $line"
+    rm -rf $line
+done < $FIND_OLD_FILES
+
