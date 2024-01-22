2d32c1b48162c2be43ae63d52cc58127cf4ad18b
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-22 20:03:42 +0530

    shell script for deleting old logs

--- 15-delete-old-log.sh
+++ 15-delete-old-log.sh
@@ -15,14 +15,15 @@ then
 fi
 
 #find . -type f -mtime +14 -name
-. -->> current working directory
-f -->> files
-m -->> morethan 
+#. -->> current working directory
+#f -->> files
+#m -->> morethan 
+
 FIND_OLD_FILES=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
 while IFS= read -r line
 do
     echo "Deleting files: $line"
     rm -rf $line
-done <<< $FIND_OLD_FILES
+done < $FIND_OLD_FILES
 
