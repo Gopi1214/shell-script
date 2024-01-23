5823a8e9380c397d10824ac0947c68874ffca568
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-23 13:04:16 +0530

    disk usage script

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -7,7 +7,7 @@ message=""
 while IFS= read -r file
 do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
-    partition=$(echo $usage | awk '{print $1F}')
+    partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $DISK_THRESHOLD ]
     then
         message+="High disk usage on $partition : $usage"
