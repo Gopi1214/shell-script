b168037727f5bed09fd888436c4695a905abfd4c
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-25 06:51:46 +0530

    disk usage

--- mail.sh
+++ mail.sh
@@ -7,6 +7,6 @@ ESCAPE_CHAR=$(printf '%s\n' "$BODY" | sed -e 's#\([]*^+\.\$[-]\)##g')
 TO_ADDRESS=$4
 SUBJECT=$5
 
-FINAL_BODY=$(sed -e "s/Team/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$BODY/g" template.html)
+FINAL_BODY=$(sed -e 's/Team/$TO_TEAM/g' -e 's/ALERT_TYPE/$ALERT_TYPE/g' -e 's/BODY/$BODY/g' template.html)
 
 echo "$FINAL_BODY" | mail -s "$(echo -e $SUBJECT\nContent-Type: text/html)" "$TO_ADDRESS"
