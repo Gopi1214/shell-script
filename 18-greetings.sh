f8ee3c60abe7c7a2dae499bb16db5cef2b9c4abb
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-26 15:01:44 +0530

    optargs

--- 18-greetings.sh
+++ 18-greetings.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 NAME=""
-WISHES=""
+WISHES="Good Morning"
 
 USAGE(){
     echo "USAGE:: $(basename $0) -n $NAME -w $WISHES"
     echo "options::"
     echo "-n, specify the name(mandatory)"
-    echo "-w, specify the wishes Ex: Good Morning"
+    echo "-w, specify the wishes (optional). Default whish is good morning"
     echo "-h, help and exit"
 }
 
@@ -18,11 +18,13 @@ while getopts ":n:w:h" opt; do
     esac
 done
 
-if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
-    echo "ERROR: -n and -w are mandatory"
+#if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
+if [ -z "$NAME" ]
+    #echo "ERROR: -n and -w are mandatory options"
+    #echo "ERROR: -n is mandatory option"
     USAGE
     exit 1
 fi
 
-echo "Hello $NAME. $WISHES I am learning shell script"
+echo "Hello $NAME. $WISHES, I am learning shell script"
 
