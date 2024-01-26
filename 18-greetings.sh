fdcd1edfe08fa367a2d6d663cf0a3f750b2b07e9
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-26 07:11:41 +0530

    optargs

--- 18-greetings.sh
+++ 18-greetings.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
-NAME=""
-WISHES=""
+# NAME=""
+# WISHES=""
 
-USAGE(){
-    echo "USAGE:: $(basename $0) -n $NAME -w $WISHES"
-    echo "options::"
-    echo "-n, specify the name(mandatory)"
-    echo "-w, specify the wishes Ex: Good Morning"
-    echo "-h, help and exit"
-}
+# USAGE(){
+#     echo "USAGE:: $(basename $0) -n $NAME -w $WISHES"
+#     echo "options::"
+#     echo "-n, specify the name(mandatory)"
+#     echo "-w, specify the wishes Ex: Good Morning"
+#     echo "-h, help and exit"
+# }
 
-while getopts ":n:w:h" opt; do
-    case "${opt}" in
-        n) NAME=${OPTARG};;
-        w) WISHES=${OPTARG};;
-        h|*) USAGE;;
-        :) USAGE;;
-    esac
-done
-
-
-
-# usage() { echo "Usage: $0 [-s <45|90>] [-p <string>]" 1>&2; exit 1; }
-
-# while getopts ":s:p:" o; do
-#    case "${o}" in
-#         s)
-#             s=${OPTARG}
-#             ((s == 45 || s == 90)) || usage
-#             ;;
-#         p)
-#             p=${OPTARG}
-#             ;;
-#         *)
-#             usage
-#             ;;
+# while getopts ":n:w:h" opt; do
+#     case "${opt}" in
+#         n) NAME=${OPTARG};;
+#         w) WISHES=${OPTARG};;
+#         h|*) USAGE;;
+#         :) USAGE;;
 #     esac
 # done
+
+
+
+usage() { echo "Usage: $0 [-s <45|90>] [-p <string>]" 1>&2; exit 1; }
+
+while getopts ":s:p:" o; do
+   case "${o}" in
+        s)
+            s=${OPTARG}
+            ((s == 45 || s == 90)) || usage
+            ;;
+        p)
+            p=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
